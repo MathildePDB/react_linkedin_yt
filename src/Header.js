462c190580de76247212d52ecc08b0543b6dd9a9
MathildePDB <mathilde.pdb@laposte.net>
2023-09-25 16:12:04 +0200

    header front

---
+++ src/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import Logo from './assets/linkedin.svg'
+import Avatar from './assets/avatar.jpg'
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home'
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount'
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter'
+import ChatIcon from '@mui/icons-material/Chat'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+
+function Header() {
+  return (
+    <div className="header">
+        <div className="header__left">
+          <img src={Logo} alt="LinkedIn logo" />
+          <div className="header__search">
+            <SearchIcon />
+            <input type="text" />
+          </div>
+        </div>
+
+        <div className="header__right">
+          <HeaderOption Icon={HomeIcon} title="Home" />
+          <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+          <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+          <HeaderOption Icon={ChatIcon} title="Messaging" />
+          <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+          <HeaderOption avatar={Avatar} title="me" />
+        </div>
+    </div>
+  )
+}
+
+export default Header
